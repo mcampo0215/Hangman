3a937c966e1d8be8a344ebf1868662ddb6db6a10
mcampo0215 <matthewcampoverde@gmail.com>
2025-10-15 10:42:17 -0400

    code to choose random word

---
+++ hangman.js
@@ -0,0 +1,5 @@
+var POSSIBLE_WORDS = ["obdurate", "defenestrate", "obsquious",
+    "dissonant", "obdurate"];
+
+var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS(randomIndex);
