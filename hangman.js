f4c28de161919697d381e8719f75ed019d71a8e8
mcampo0215 <matthewcampoverde@gmail.com>
2025-10-20 10:33:54 -0400

    made edit guess letter functionality

--- hangman.js
+++ hangman.js
@@ -1,16 +1,34 @@
 var POSSIBLE_WORDS = ["obdurate", "defenestrate", "obsquious",
-    "dissonant", "obdurate"];
+    "dissonant", "obdurate", "toady"];
 
 var word = "";
-
+var guesses = "";
 function newGame() {
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
-    
+    guesses = "";
+    updatePage();
+}
+
+function guessLetter() {
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses += letter;
+    updatePage();
+}
+
+function updatePage() {
     var clueString = "";
     for(var i = 0; i < word.length; i++) {
-        clueString += "_ ";
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >= 0) {
+            clueString += currentLetter + " ";
+        }
+        else
+            clueString += "_ ";
     }
-    var clue = document.getElementById("clue");
+     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: "  + guesses;
 }
